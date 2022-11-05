42ff689fafbd1dc491bd7cfb599408a7306904aa
akinori <aki10.working@gmail.com>
2022-11-05 19:35:00 +0900

    fix: modified contact mail text

--- src/contents/common/index.ts
+++ src/contents/common/index.ts
@@ -11,6 +11,6 @@ export const company = {
   name: '株式会社imp',
   address: '〒166-0012 東京都杉並区和田2-33-5',
   tel: '080-8499-7385',
-  email: 'contact@impre.jp',
+  email: 'info@drone-programming.impre.jp',
   url: 'https://impre.jp'
 }
